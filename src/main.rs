cc7cf61a945a4ca99f7048da8c88838076dee1dd
5-23 <yhanbyeol6@gmail.com>
2023-03-13 19:02:41 +0900

    make init

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_system(setup)
+        .run();
+}
+
+struct Element;
+impl Default for Element{
+    fn default() -> Self {
+        Self{}
+    }
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>
+){
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: meshes.add(shape::Circle::new(50.).into()).into(),
+        material: materials.add(ColorMaterial::from(Color::PURPLE)),
+        transform: Transform::from_translation(Vec3::new(-150., 0., 0.)),
+        ..default()
+    });
+}
